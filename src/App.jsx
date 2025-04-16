80fbfd43ec38e05f0d2a30f8ca68e05a034a2da4
CODE-WITH-DEVESH <dksbaghel4@gmail.com>
2025-04-16 09:51:27 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Chai from './Chai'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,8 @@ function App() {
     <>
      <h1>Welcome</h1>
      <button>Submit </button>
+     <button>Add Your Value</button>
+     <Chai/>
     </>
   )
 }
---
+++ src/Chai.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Chai = () => {
+  return (
+    <>
+   <h2>ADD TWO </h2>
+   <h2></h2>
+    </>
+  )
+}
+
+export default Chai
